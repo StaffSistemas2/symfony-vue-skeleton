e4a258d9a70ef30e36f6cbd2c4387ef8c66589c9
Francisco Amador <practicante.sistemas@mexq.com.mx>
2020-08-31 09:31:35 -0500

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var Encore = require('@symfony/webpack-encore');
+const { VueLoaderPlugin } = require('vue-loader');
+
+Encore
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  // .enableVersioning(Encore.isProduction())
+  .addEntry('js/app', './assets/js/index.js')
+  .addStyleEntry('css/style', './assets/css/custom.scss')
+  .enableSassLoader()
+  .enableVueLoader()
+// .addPlugin(new VueLoaderPlugin())
+// .addAliases({
+//   vue: 'vue/dist/vue.js'
+// });
+
+module.exports = Encore.getWebpackConfig();
